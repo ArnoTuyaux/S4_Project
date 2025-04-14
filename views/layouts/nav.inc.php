acfed7595f724d990c88f45e8fad9daa10a36c53
Richard <richardjules12@gmail.com>
2025-04-14 12:27:52 +0200

    changement de nav bar

--- views/layouts/nav.inc.php
+++ views/layouts/nav.inc.php
@@ -1,3 +1,12 @@
+<!--nav.inc.php-->
 <div class="navbar">
     <a href="index.php">Accueil</a>
+</div>
+
+<div class="side-navbar">
+    <a href="index.php?page=barmanAccueil">Gestion des tables</a>
+    <a href="index.php?page=barmanAccueil">Réservation</a>
+<!--    <button onclick="openReservationModal()">Réservation</button>-->
+    <a href="index.php?page=boisson">Boisson</a>
+    <a href="index.php?page=parametres" class="settings-btn">⚙️</a>
 </div>
